2f0f54bfa36ad52cbe28f3c73535aad921fbb78d
gys619 <1554537386@qq.com>
2021-11-05 18:45:54 +0800

    update

--- jd_joy_park_task.js
+++ jd_joy_park_task.js
@@ -40,7 +40,9 @@ $.invitePin = [
   "7zG4VHS99AUEoX1mQTkC9Q",
   "BbsjCRrQudIL06kRvqmVln053h03GiApg7HN_Vhy_Og",
   "sAxL-dc5T6lS6wtKqP6SlA",
-  "bcVxt4PbZdbX7tiT1Q_ubg"
+  "bcVxt4PbZdbX7tiT1Q_ubg",
+  "_p6gjdwSemuAldidzb9l3xr4PUYxQ0IiGIpDIekNW0w",
+  "JT2qgWwSZ4TeP7tuwhgUDw"
 ]
 const JD_API_HOST = `https://api.m.jd.com/client.action`;
 message = ""
