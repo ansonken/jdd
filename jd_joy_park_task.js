a6cf148dd255a7e0e719eca0fa3824920675bf93
gys619 <1554537386@qq.com>
2021-11-13 05:39:08 +0800

    修改内容：[ jd_joy_park_task.js, jd_superBrand.js, jx_sign.js]

--- jd_joy_park_task.js
+++ jd_joy_park_task.js
@@ -35,11 +35,6 @@ if ($.isNode()) {
 }
 $.invitePinTaskList = []
 $.invitePin = [
-  "",
-  "",
-  "",
-  "",
-  "",
   ""
 ]
 const JD_API_HOST = `https://api.m.jd.com/client.action`;
@@ -151,8 +146,9 @@ message = ""
             }
           }
         } else if (task.taskType === 'SHARE_INVITE') {
+          $.yq_taskid = task.id
           for (let j = 0; j < 5; j++) {
-            let resp = await apTaskDrawAward(261, 'SHARE_INVITE');
+            let resp = await apTaskDrawAward($.yq_taskid, 'SHARE_INVITE');
 
             if (!resp.success) {
               break
@@ -166,9 +162,9 @@ message = ""
           $.log(`${task.taskTitle}|${task.taskShowTitle} 领取奖励`)
           await apTaskDrawAward(task.id, task.taskType);
         }
-        if (task.taskType === 'SHARE_INVITE') {
-          $.yq_taskid = task.id
-        }
+        // if (task.taskType === 'SHARE_INVITE') {
+        //   $.yq_taskid = task.id
+        // }
       }
     }
   }
